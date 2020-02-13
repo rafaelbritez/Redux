c6d575328734d9604af47a24c6c81590d057eac2
rafaelbritez <rafael_amonteiro@hotmail.com>
2020-02-13 12:36:33 -0300

    Redux arquitetura flux

---
+++ src/pages/Checkout/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container} from './styles';
+
+
+
+export default function Checkout(){
+
+return (
+ <Container>
+  <h1>Hello world</h1>
+
+ </Container>
+);
+
+}
---
+++ src/pages/Checkout/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+background: #fff;
+padding:30px;
+border-radius:4px;
+`;
